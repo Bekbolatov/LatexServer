e827b74764f1d6239cfce4e685fd8936f3b83a58
Renat Bekbolatov <rbekbolatov@avvo.com>
2016-07-25 01:05:11 -0700

    cleanup latex service

--- latex_service/build.sbt
+++ latex_service/build.sbt
@@ -1,14 +1,9 @@
-import play.sbt.PlayScala
-
 name := """latex-service-client"""
 organization := "com.sparkydots"
 version := "1.0-SNAPSHOT"
 scalaVersion := "2.11.8"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
-
-libraryDependencies += ws
-
-libraryDependencies += "com.sparkydots" %% "service-discovery" % "1.0-SNAPSHOT"
-
-unmanagedSourceDirectories in Compile += baseDirectory.value / "src" / "main" / "scala"
+libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play-ws" % "2.5.4" % "provided",
+  "com.sparkydots" %% "service-discovery" % "1.0-SNAPSHOT"
+)
---
+++ latex_service/src/main/scala/com/sparkydots/service/latex/LatexService.scala
@@ -0,0 +1,85 @@
+package com.sparkydots.service.latex
+
+import javax.inject.{Inject, Singleton}
+
+import com.sparkydots.service.{ServiceInstance, ServiceDiscovery}
+import play.api.http.HttpEntity
+
+import scala.concurrent.{Future, ExecutionContext, Promise}
+import scala.concurrent.duration._
+import play.api.libs.ws._
+import play.api.mvc.{Result, ResponseHeader}
+import play.api.mvc.Results.Ok
+
+
+@Singleton
+class LatexService @Inject()(ws: WSClient, serviceDiscovery: ServiceDiscovery) {
+
+  def convertLatex(tex: String, serviceInstance: ServiceInstance,
+                   timeoutMillis: Long = 10000,
+                   log: Option[(=> String) => Unit] = None
+                  )
+                  (implicit ec: ExecutionContext): Future[(Boolean, String)] =
+    ws.
+      url(serviceInstance.httpUrl("/cgi-bin/latex2pdf.sh")).
+      withHeaders("Content-Type" -> "application/x-tex").
+      withHeaders("Accept" -> "application/json").
+      withRequestTimeout(timeoutMillis.millis).
+      post(tex).
+      map { response =>
+        val jresponse = response.json
+        if ((jresponse \ "error").toOption.isDefined)
+          (false, serviceInstance.httpUrl((jresponse \ "log").as[String]))
+        else
+          (true, serviceInstance.httpUrl((jresponse \ "uri").as[String]))
+      }
+
+  def convertLatexFile(tex: String,
+                       timeoutMillis: Long = 10000,
+                       log: Option[(=> String) => Unit] = None)
+                      (implicit ec: ExecutionContext): Future[(Boolean, Result)] = {
+
+    val serviceProduct = serviceDiscovery.call[(Boolean, Result)]("latex2pdf", log = log) { serviceInstance =>
+
+      val promise = Promise[Option[(Boolean, Result)]]()
+
+      val futureResponse: Future[(Boolean, WSResponse)] = for {
+        (success, uri) <- convertLatex(tex, serviceInstance, timeoutMillis = timeoutMillis, log = log)
+        fileResponse <- ws.url(uri).get()
+      } yield (success, fileResponse)
+
+      val futureResult: Future[Option[(Boolean, Result)]] = futureResponse.map { case (success, response) =>
+        Some((success, if (success) {
+          Result(
+            header = ResponseHeader(200),
+            body = HttpEntity.Strict(response.bodyAsBytes, Some("application/pdf"))
+          ).withHeaders("Content-Disposition" -> "inline; filename=\"result.pdf\"")
+        } else {
+          Result(
+            header = ResponseHeader(200),
+            body = HttpEntity.Strict(response.bodyAsBytes, Some("plain/text"))
+          )
+        })
+        )
+      }
+
+      futureResult.onSuccess { case res =>
+          promise.success(res)
+      }
+
+      futureResult.onFailure { case t =>
+        log.foreach(_("LatexService: Error getting from server"))
+        log.foreach(_(t.getStackTrace.toString))
+        promise.success(None)
+      }
+
+      promise.future
+    }
+
+    serviceProduct.map {
+      case Some(r) => r
+      case None => (false, Ok("Server error"))
+    }
+  }
+
+}
